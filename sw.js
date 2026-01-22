70a1dd976ff09b67efef864550814910a318cb77
Marius Gundersen <marius.gundersen@clave.no>
2026-01-22 08:12:47 +0100

    new cache

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
+const CACHE = "v1";
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("number-tap-game-cache").then((cache) => {
+    caches.open(CACHE).then((cache) => {
       return cache.addAll(["/index.html", "/styles.css", "/script.js"]);
     }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)),
+        ),
+      ),
   );
 });
 
